64e988008a5bdd4686d5f19f0e3aa75ba7ec184a
Laura Marchand <marchand_laura@outlook.fr>
2020-01-21 02:30:48 -0800

    fix(front): add CryptoMenu

---
+++ frontend/src/components/UserBanner/CryptosMenu/CryptosMenu.js
@@ -0,0 +1,39 @@
+import './cryptosmenu.css'
+
+import React from 'react';
+import icon from './graph_icon.svg'
+import { UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap'
+
+class CryptosMenu extends React.Component {
+  render() {
+    return (
+      <span className="cryptosmenu">
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+          <img src={icon} alt="cryptos_menu_icon"
+            longdesc="made by https://www.flaticon.com/authors/good-ware"
+            className="icon"/>
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              My Favourites
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Trending
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              All cryptos
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </span>
+    );
+  }
+}
+
+export default CryptosMenu;
