7f3b740c2206e4943715a0f2476e00fd75933d15
julien <julien.legay@epitech.eu>
2020-01-27 16:48:38 +0100

    Crypto API first release + store ORM

--- backend/index.js
+++ backend/index.js
@@ -1,82 +1,19 @@
+
 /**
  * Cryptolabz
  * 2020 - EPITECH PROJECT
  */
 
-//const Logger = require('./mods/log/logger.js').Logger;
-//const Config = require(__dirname + "/mods/config.js");
-//const Server = require('./mods/server.js');
-//const Logger = require('./mods/log/logger.js').Logger
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger')
-const schedule = require('node-schedule')
+const Logger = require(__dirname + '/mods/log/logger.js').Logger;
+const Config = require(__dirname + "/mods/config.js");
+const Server = require(__dirname + '/mods/server.js');
+const CoinbaseAPI = require(__dirname+"/mods/scheduler/coinbase");
 
-
-const axios = require('axios')
-
-const config = {
-  name: 'cryptolabzAPI',
-  port: 3000,
-  host: '0.0.0.0'
-}
-
-/*var db = require('./mods/ORM/database.js');
 Server.listen(Config.httpserver.port, () => {
     console.log("Cryptolabz server started");
     Logger.log("Cryptolabz Server started.");
-})*/
-
-const app = express()
-const logger = log({ console: true, file: false, label: config.name})
-
-app.use(bodyParser.json())
-app.use(cors())
-app.use(ExpressAPILogMiddleware(logger, {request: true}))
-
-
-
-var currencies = []
-
-schedule.scheduleJob('*/10 * * * * *', () => {
-  axios.get("https://www.coinbase.com/api/v2/assets/search?base=EUR&filter=all&include_prices=true&limit=10&order=asc&query=&resolution=day&sort=rank")
-    .then(response => {
-      currencies = []
-      var result = response.data.data
-      result.forEach(element => {
-        currencies
-          .push({ name: element.name,
-                  symbol: element.symbol,
-                  slug: element.slug,
-                  currentPrice: element.latest + ' €',
-                  imageUrl: element.image_url
-                })
-      });
-      console.log(currencies)
-    })
-    .catch(error => {
-      console.log(error)
-    })
 })
 
-
-
-
-app.listen(config.port, config.host, (e) => {
-    if(e) {
-        throw new Error('Internal Server Error')    
-    }
-    logger.info(`${config.name} running on ${config.host}:${config.port}`) 
-})
-
-
-/*db.User.findAll().then(users => {
-  console.log(users);
-})/*
-db.User.create({ email:"test@example.fr", password: "caca", perm_level: 1, username: "Jogny", firstname: "Jane", lastname: "Doe" }).then(jane => {
-  console.log("Jane's auto-generated ID:", jane.id);
-});/*
-User.findAll().then(users => {
-  console.log("All users:", JSON.stringify(users, null, 4));
-});*/
+// LAUNCHING SCHEDULED TASKS
+var coinbaseApi = new CoinbaseAPI(60);
+coinbaseApi.start();
---
+++ backend/mods/scheduler/coinbase.js
@@ -0,0 +1,66 @@
+/**
+ * CRYPTOLABZ 2020
+ * Last modified: 27/01/2020
+ * EPITECH PROJECT
+ */
+
+//const schedule = require('node-schedule')
+const axios = require('axios')
+const CryptosController = require(__dirname+"/../controller/cryptoscontroller")
+var cryptosController = new CryptosController();
+
+class CoinbaseAPI {
+
+    constructor(speed) {
+        console.log("COINBASE | API Class Loaded")
+        if (!speed) speed = 30
+        this.speed = speed;
+    }
+
+    start() {
+        if (!this.isStarted()) {
+            console.log("COINBASE | Coinbase interval API Started")
+            this.started = true
+            this.perform()
+            setInterval(() => {
+                this.perform()
+              }, this.speed * 1000)
+        } else {
+            return false;
+        }
+    }
+
+    perform() {
+        axios.get("https://www.coinbase.com/api/v2/assets/search?base=EUR&filter=all&include_prices=true&limit=100&order=asc&query=&resolution=day&sort=rank")
+        .then(response => {
+          var result = response.data.data
+          result.forEach(element => {
+          cryptosController
+              .update({ name: element.name,
+                      symbol: element.symbol,
+                      slug: element.slug,
+                      currentPrice: element.latest,
+                      imageUrl: element.image_url
+                    })
+          });
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+
+    stop() {
+        if (this.isStarted()) {
+            console.log("COINBASE | API stopped")
+            this.scheduled.cancel();
+            this.started = false;
+        } else 
+            return false;
+    }
+
+    isStarted() {
+        return this.started;
+    }
+}
+
+module.exports = CoinbaseAPI;
