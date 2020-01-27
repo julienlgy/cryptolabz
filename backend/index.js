71642868ee649290708cfab3ebf4ae495b3ecf8f
Administrator <raphael.palerme@epitech.eu>
2020-01-27 15:09:21 +0100

    Currencies scheduleJob

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger')
+const schedule = require('node-schedule')
+
+
+const axios = require('axios')
 
 const config = {
   name: 'cryptolabzAPI',
@@ -31,9 +35,33 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(ExpressAPILogMiddleware(logger, {request: true}))
 
-app.get('/', (req, res) => {
-    res.status(200).send('hello word')
+
+
+var currencies = []
+
+schedule.scheduleJob('*/10 * * * * *', () => {
+  axios.get("https://www.coinbase.com/api/v2/assets/search?base=EUR&filter=all&include_prices=true&limit=10&order=asc&query=&resolution=day&sort=rank")
+    .then(response => {
+      currencies = []
+      var result = response.data.data
+      result.forEach(element => {
+        currencies
+          .push({ name: element.name,
+                  symbol: element.symbol,
+                  slug: element.slug,
+                  currentPrice: element.latest + ' €',
+                  imageUrl: element.image_url
+                })
+      });
+      console.log(currencies)
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
+
+
+
 
 app.listen(config.port, config.host, (e) => {
     if(e) {
