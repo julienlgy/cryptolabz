b2564a6d7d8d4c590bac1d60de2310e3a043c390
= <=>
2020-02-01 13:10:36 +0100

    top10

--- frontend/src/components/Cryptolabz/Cryptolabz.js
+++ frontend/src/components/Cryptolabz/Cryptolabz.js
@@ -7,9 +7,14 @@ class Cryptolabz extends React.Component {
   render() {
     return (
       <div className="Cryptolabz">
+        <div class="cryptolabz text-center main-title">
+          <h1 class="font-weight-bold">Become a crypto scientist !</h1>
+        </div>
         <div class="container-fluid">
-          <div class="row cryptolabz">
+          <div class="row justify-content-around cryptolabz">
             <Top10Crypto />
+          </div>
+          <div class="row justify-content-around cryptolabz">
             <News />
           </div>
         </div>
--- frontend/src/components/UserBanner/UserBanner.js
+++ frontend/src/components/UserBanner/UserBanner.js
@@ -1,17 +1,15 @@
-import './userbanner.css'
+import "./userbanner.css";
 
-import React from 'react';
-import CryptosMenu from './CryptosMenu/CryptosMenu'
-import UserMenu from './UserMenu/UserMenu'
-import SearchBar from './SearchBar/SearchBar'
-import { Button,
-  Nav,
-  Navbar } from 'reactstrap'
+import React from "react";
+import CryptosMenu from "./CryptosMenu/CryptosMenu";
+import UserMenu from "./UserMenu/UserMenu";
+import SearchBar from "./SearchBar/SearchBar";
+import { Button, Nav, Navbar } from "reactstrap";
 
 class UserBanner extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { isAuthentified: false }
+    super(props);
+    this.state = { isAuthentified: false };
   }
 
   handleClickSignIn() {
@@ -20,14 +18,14 @@ class UserBanner extends React.Component {
       isAuthentified: true
     });
   }
-  
+
   handleClickSignUp() {
     this.setState({
       ...this.state,
       isAuthentified: true
     });
   }
-  
+
   handleClickSignOut() {
     this.setState({
       ...this.state,
@@ -35,62 +33,75 @@ class UserBanner extends React.Component {
     });
   }
 
-  handleUpdateSearch = (search_string) => {}
+  handleUpdateSearch = search_string => {};
 
-  handleDoSearch = (search_string) => {
-    console.log("todo: do search " + search_string)
-  }
+  handleDoSearch = search_string => {
+    console.log("todo: do search " + search_string);
+  };
 
   renderSignIn() {
-    return(
-      <Button type="button" color="dark"
-        onClick={() => this.handleClickSignIn()}>
+    return (
+      <Button
+        type="button"
+        color="dark"
+        onClick={() => this.handleClickSignIn()}
+      >
         Sign in
       </Button>
-    )
+    );
   }
 
   renderSignUp() {
-    return(
-      <Button type="button" color="dark"
-        onClick={() => this.handleClickSignUp()}>
+    return (
+      <Button
+        type="button"
+        color="dark"
+        onClick={() => this.handleClickSignUp()}
+      >
         Sign up
       </Button>
-    )
+    );
   }
 
   renderSignOut() {
-    return(
-      <Button type="button" color="dark"
-        onClick={() => this.handleClickSignOut()}>
+    return (
+      <Button
+        type="button"
+        color="dark"
+        onClick={() => this.handleClickSignOut()}
+      >
         Sign out
       </Button>
-    )
+    );
   }
 
   render() {
     return (
       <div class="userbanner">
-        <Navbar color="dark" expand="md" fixed="top"
-          className="userbanner navbar-default navbar-size">
-            <div className="mr-auto">
-              <span className="cryptolabz">
-                Cryptolabz
-              </span>
-            </div>
+        <Navbar
+          color="dark"
+          expand="md"
+          fixed="top"
+          className="userbanner navbar-default navbar-size"
+        >
+          <div className="mr-auto">
+            <span className="cryptolabz">Cryptolabz</span>
+          </div>
 
-            <Nav>
-              { !this.state.isAuthentified && this.renderSignIn() }
-              { !this.state.isAuthentified && this.renderSignUp() }
+          <Nav>
+            {!this.state.isAuthentified && this.renderSignIn()}
+            {!this.state.isAuthentified && this.renderSignUp()}
 
-              { this.state.isAuthentified && <SearchBar
-                  onUpdateSearch={this.handleUpdateSearch}
-                  onDoSearch={this.handleDoSearch}/> }
-              { this.state.isAuthentified && <CryptosMenu /> }
-              { this.state.isAuthentified && <UserMenu /> }
-              { this.state.isAuthentified && this.renderSignOut() }
-
-            </Nav>
+            {this.state.isAuthentified && (
+              <SearchBar
+                onUpdateSearch={this.handleUpdateSearch}
+                onDoSearch={this.handleDoSearch}
+              />
+            )}
+            {this.state.isAuthentified && <CryptosMenu />}
+            {this.state.isAuthentified && <UserMenu />}
+            {this.state.isAuthentified && this.renderSignOut()}
+          </Nav>
         </Navbar>
         <div class="navbar-size"></div>
       </div>
