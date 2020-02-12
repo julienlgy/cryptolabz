f8fe37f0795aec04832621fa74d386c008bc2496
JL <=>
2020-02-12 16:36:22 +0100

    HomePage dynamic values

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,8 +1,9 @@
-const BASE_URL = "http://127.0.0.1:8000"
+const BASE_URL = "http://localhost:8000"
 const ROUTE_CRYPTO = "/cryptos?cmids="
 const ROUTE_LOGIN = "/users/login"
 const ROUTE_REGISTER = "/users/register"
 const ROUTE_UPDATE = "/users/update"
+const ROUTE_HOMEPAGE = "/cryptos/get/home"
 
 class API {
   url = BASE_URL
@@ -12,6 +13,7 @@ class API {
   url_update = BASE_URL + ROUTE_UPDATE
 
   url_crypto = BASE_URL + ROUTE_CRYPTO
+  url_crypto_home = BASE_URL + ROUTE_HOMEPAGE
 }
 
 export default API = new API()
