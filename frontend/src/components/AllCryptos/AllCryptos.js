c229956729e3d469716c66d4d957724116cf0959
Laura Marchand <marchand_laura@outlook.fr>
2020-02-13 09:05:57 -0800

    dev(front): impr page that lists all cryptocurrencies

--- frontend/src/components/AllCryptos/AllCryptos.js
+++ frontend/src/components/AllCryptos/AllCryptos.js
@@ -2,13 +2,8 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./allcryptos.css";
 
-import { Button,
-  Col,
-  Container,
-  ListGroup,
-  ListGroupItem,
-  Row } from 'reactstrap';
-
+import Pagination from "react-js-pagination";
+import { Col, Container } from 'reactstrap';
 import axios from "axios";
 import API from "./../../API"
 
@@ -17,14 +12,21 @@ class AllCryptos extends React.Component {
       super(props);
       this.state = {
         cryptos: [],
+        cryptosToDisplay: [],
+        numberCryptosPerPage: 15,
+        numberPage: 1,
       }
     }
 
     componentDidMount() {
       axios.get(API.url_crypto_all)
       .then(response => {
+        let toDisplay = response.data.data.slice(
+            (this.state.numberPage - 1) * this.state.numberCryptosPerPage,
+            this.state.numberPage * this.state.numberCryptosPerPage)
         this.setState({ 
           cryptos: response.data.data,
+          cryptosToDisplay: toDisplay
         });
       })
       .catch(error => {
@@ -32,22 +34,46 @@ class AllCryptos extends React.Component {
       });
     }
 
+    handlePageChange = (pageNumber) => {
+      let toDisplay = this.state.cryptos.slice(
+          (pageNumber - 1) * this.state.numberCryptosPerPage,
+          pageNumber * this.state.numberCryptosPerPage)
+      this.setState({
+        cryptosToDisplay: toDisplay,
+        numberPage: pageNumber
+      });
+    }
+
     render() {
       return (
-      <div className="admincryptos">
-        <h1>Admin Cryptocurrencies</h1>
-        <ul>
-          {this.state.cryptos.map((crypto,index) =>
-          <div
+      <div className="allcryptos">
+        <h1>Cryptocurrencies</h1>
+        <Container>
+          {this.state.cryptosToDisplay.map((crypto,index) =>
+          <Col
+              xs={{ size: 3}}
+              className="list-inline-item align-top"
               key={index}>
             <img
                 className="icon"
                 alt={"image_" + crypto.symbol}
                 src={crypto.imgUrl} />
+            <div className="label">
               {crypto.symbol + " - " + crypto.name}
-          </div>
+            </div>
+          </Col>
           )}
-        </ul>
+        </Container>
+        <Pagination
+          activePage={this.state.numberPage}
+          itemsCountPerPage={this.state.numberCryptosPerPage}
+          totalItemsCount={this.state.cryptos.length}
+          pageRangeDisplayed={5}
+          onChange={this.handlePageChange}
+          innerClass="pagination justify-content-center"
+          itemClass="page-item"
+          linkClass="page-link"
+        />
       </div>
     );
   }
